25b3961f6c4101f8d818cae2207165f9f5616b48
Taben Malik <malik@arlut.utexas.edu>
2023-08-24 11:34:54 -0500

    Making gnsstk-apps_enable.sh source commands POSIX compliant.

--- debian/gnsstk-apps_enable.sh
+++ debian/gnsstk-apps_enable.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # This file should be sourced and not executed as bash script.  Ex. source /etc/profile.d/gnsstk-apps_enable.sh
 GNSSTK_APPS_VER=__VER__
 
@@ -9,11 +8,11 @@ FILE=/etc/toolkits.conf
 
 if [ -f "$FILE" ]; then
     echo "found $FILE so sourcing it"
-    set -o allexport && source $FILE && set +o allexport
+    set -o allexport && . $FILE && set +o allexport
 fi
 
 echo "gnsstk-apps major version being used is $GNSSTK_APPS_VER"
-source /usr/share/gnsstk__VER__-apps/enable
+. /usr/share/gnsstk__VER__-apps/enable
 
 echo -e "\nThe following env variables have been set or are already set"
 env | grep 'LD_LIBRARY_PATH\|CPLUS_INCLUDE_PATH\|PYTHONPATH\|^PATH'
